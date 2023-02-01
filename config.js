2f99dc87712a8e14fc6426df073667ad209e78e1
Syed Abdulrab <99114574+SyedAbdulrab@users.noreply.github.com>
2023-02-01 15:32:37 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB_URL:
+    "mongodb+srv://syed_abdulrab:syedabdulrab@cluster0.nt7qb.mongodb.net/merng?retryWrites=true&w=majority",
+    SECRET_KEY:"some very secret key"
+};
